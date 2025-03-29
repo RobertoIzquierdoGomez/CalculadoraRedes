fee0da40b88ec665f109936afe15fe860d920aaf
Rober <robert1993_sira@hotmail.com>
2025-03-29 15:59:23 +0100

    Updated explication.js

--- Scripts/explication.js
+++ Scripts/explication.js
@@ -4,21 +4,24 @@ document.querySelector("#aExplicacion").addEventListener("click", function() {
     document.getElementById("main").innerHTML = "";
     document.getElementById("main").innerHTML = `<div id="intro">
             <h2> Explicación</h2>
-            <p> Los rangos de IP van desde <strong>0.0.0.0</strong> a <strong>255.255.255.255</strong> en este proyecto nos centramos únicamente en IPv4</p>
-            <p>En función de la IP se dividen en distintas clases que sirven para identificar su <strong>máscara de red.</strong> La máscara de red sirve para identificar qué parte de la IP pertenece a <strong class="red">red</strong> y qué parte a <strong class="host">host.</strong></p>
+            <p>Las direcciones IP en formato IPv4 van desde <strong>0.0.0.0</strong> hasta <strong>255.255.255.255</strong>. En este proyecto nos centramos únicamente en el uso de direcciones IPv4.</p>
+            <p>Estas direcciones se dividen en distintas <strong>clases</strong> (A, B y C), que determinan el tamaño de la red y cuántos dispositivos pueden conectarse. Esta clasificación se basa en el primer número de la IP.</p>
+
+            <p>Cada IP tiene asociada una <strong>máscara de red</strong>, que nos permite saber qué parte de la dirección identifica la <strong class="red">red</strong> y qué parte identifica al <strong class="host">host</strong> (el dispositivo dentro de la red).</p>
+            <p>Por ejemplo, en una IP como <code>192.168.1.10</code> con una máscara <code>255.255.255.0</code>, los primeros tres bloques (<code>192.168.1</code>) indican la red, y el último bloque (<code>.10</code>) representa el host.</p>
             <h3>Clases de IP</h3>
             <ul>
                 <li><strong>Clase A:</strong> rango de <strong class="red">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> a <strong class="red">127</strong>.<strong class="host">255</strong>.<strong class="host">255</strong>.<strong class="host">255</strong></li>
                     <ul>
                         <li><strong>Máscara de red: </strong> <strong class="red">255</strong>.<strong class="host">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong></li>
                     </ul>
-                <li><strong>Clase B:</strong> rango de <strong class="red">128</strong>.<strong class="host">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> a <strong class="red">191</strong>.<strong class="host">255</strong>.<strong class="host">255</strong>.<strong class="host">255</strong></li>
+                <li><strong>Clase B:</strong> rango de <strong class="red">128</strong>.<strong class="red">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> a <strong class="red">191</strong>.<strong class="red">255</strong>.<strong class="host">255</strong>.<strong class="host">255</strong></li>
                     <ul>
-                        <li><strong>Máscara de red: </strong> <strong class="red">255</strong>.<strong class="host">255</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> </li>
+                        <li><strong>Máscara de red: </strong> <strong class="red">255</strong>.<strong class="red">255</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> </li>
                     </ul>
-                <li><strong>Clase C:</strong> rango de <strong class="red">192</strong>.<strong class="host">0</strong>.<strong class="host">0</strong>.<strong class="host">0</strong> a <strong class="red">224</strong>.<strong class="host">255</strong>.<strong class="host">255</strong>.<strong class="host">255</strong></li>
+                <li><strong>Clase C:</strong> rango de <strong class="red">192</strong>.<strong class="red">0</strong>.<strong class="red">0</strong>.<strong class="host">0</strong> a <strong class="red">224</strong>.<strong class="red">255</strong>.<strong class="red">255</strong>.<strong class="host">255</strong></li>
                     <ul>
-                        <li><strong>Máscara de red: </strong> </strong> <strong class="red">255</strong>.<strong class="host">255</strong>.<strong class="host">255</strong>.<strong class="host">0</strong> </li>
+                        <li><strong>Máscara de red: </strong> </strong> <strong class="red">255</strong>.<strong class="red">255</strong>.<strong class="red">255</strong>.<strong class="host">0</strong> </li>
                     </ul>
             </ul>
         </div>`;
